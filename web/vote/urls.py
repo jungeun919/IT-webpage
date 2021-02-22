32ce31da70c9e699c283a9e8b08b94a7343f8e41
jungeun919 <renee8369@likelion.org>
2021-02-22 16:48:44 +0900

    vote complete

--- web/vote/urls.py
+++ web/vote/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('<int:question_id>', vote_detail, name="vote_detail"),
     path('vote/', vote, name="vote"),
     path('vote_result/<int:question_id>/', vote_result, name="vote_result"),
+    path('q_register/', q_register, name="q_register"),
+    path('q_delete<int:question_id>', q_delete, name="q_delete"),
+    path('c_register/', c_register, name="c_register"),
+    path('c_delete<int:choice_id>', c_delete, name="c_delete"),
 ]
--- web/vote/views.py
+++ web/vote/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse
+from .forms import QuestionForm, ChoiceForm
+from datetime import datetime
 
 # Create your views here.
 def vote_list(request):
@@ -19,7 +21,43 @@ def vote(request):
         choice.votes += 1
         choice.save()
         return HttpResponseRedirect(reverse('vote_result', args=(choice.q.id,)))
-        
+
 def vote_result(request, question_id):
     ques = get_object_or_404(Question, id=question_id)
     return render(request, 'vote_result.html', {'ques':ques})
+
+def q_register(request):
+    if request.method == "GET":
+        form = QuestionForm()
+        return render(request, 'q_register.html', {'form':form})
+
+    elif request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            q = form.save(commit=False)
+            q.date = datetime.now()
+            q.save()
+            return HttpResponseRedirect(reverse('vote_list'))
+
+def q_delete(request, question_id):
+    q = get_object_or_404(Question, id=question_id)
+    q.delete()
+    return HttpResponseRedirect(reverse('vote_list'))
+
+def c_register(request):
+    if request.method == "GET":
+        form = ChoiceForm()
+        return render(request, 'c_register.html', {'form':form})
+
+    elif request.method == "POST":
+        form = ChoiceForm(request.POST)
+        if form.is_valid():
+            c = form.save()
+            return HttpResponseRedirect(reverse('vote_detail', args=(c.q.id,)))
+        else:
+            return render(request, 'c_register.html', {'form':form, 'error':'유효하지 않은 값입니다.'})
+
+def c_delete(request, choice_id):
+    c = get_object_or_404(Choice, id=choice_id)
+    c.delete()
+    return HttpResponseRedirect(reverse('vote_detail', args=(c.q.id,)))
