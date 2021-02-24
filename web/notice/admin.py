bdd8899cf50e7770ddb15f3843206349c4187422
jisoolee11 <jisu6414@likelion.org>
2021-02-24 22:04:35 +0900

    users, hit, paginator

--- web/notice/admin.py
+++ web/notice/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 
+        'writer', 
+        'hits',
+        'pub_date',
+        )
+    search_fields = ('title', 'content')
+    
 admin.site.register(Post)
---
+++ web/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# app_name = 'users'
+
+urlpatterns = [
+    # path('agreement/', views.AgreementView.as_view(), name='agreement'),
+    path('csregister/', views.CsRegisterView.as_view(), name='csregister'), # 추가
+]
